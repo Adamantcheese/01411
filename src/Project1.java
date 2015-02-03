c1fd0e879c04565e7a0811865f4c534f898ba67a
Adamantcheese <random123x@gmail.com>
2015-02-02 23:07:19 -0800

    uncommented some stuff

--- src/Project1.java
+++ src/Project1.java
@@ -8,15 +8,15 @@ public class Project1 {
         ToyScanner scanner, scanner2;
         try {
             scanner = new ToyScanner(175);
-            //scanner2 = new ToyScanner(275);
+            scanner2 = new ToyScanner(275);
             System.out.println("PRIMARY TEST FILE OUTPUT\n--------------------");
             scanner.scanFile("ToyTestFile.toy", true);
             System.out.println("\nSECONDARY TEST FILE OUTPUT\n--------------------");
-            //scanner2.scanFile("ToyTestFile2.toy", true);
+            scanner2.scanFile("ToyTestFile2.toy", true);
             System.out.println("\nSYMBOL TABLE AFTER TEST FILE 1\n--------------------");
             scanner.showTables();
             System.out.println("\nSYMBOL TABLE AFTER TEST FILE 2\n--------------------");
-            //scanner2.showTables();
+            scanner2.showTables();
         } catch (FileNotFoundException e){
             System.out.println("Couldn't find given file to parse. ");
             e.printStackTrace();
